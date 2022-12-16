6f7f2b57f4869427304d8d0cc377a6b38530a061
RKDigital <RKDigital@DESKTOP-269OAN9>
2022-12-16 10:36:57 +0530

    first commit

---
+++ src/com/DemoProject/com/FirstDemoProject.java
@@ -0,0 +1,12 @@
+package com.DemoProject.com;
+
+public class FirstDemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Demo Project");
+
+	}
+
+}
